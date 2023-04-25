cd2e516b3ac7f233ff36dedb781f61bd11fd0327
salma197457 <salma.ali67815@gmail.com>
2023-04-25 12:59:50 -0700

    project

--- convert_num.c
+++ convert_num.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_hex - prints hex numbers in lowercase
  * @ap: p1
@@ -27,6 +28,7 @@ int print_hex(va_list ap, params_t *params)
 	params->unsign = 1;
 	return (c += print_number(str, params));
 }
+
 /**
  * print_HEX - prints hex numbers in uppercase
  * @ap: p1
@@ -72,6 +74,7 @@ int print_binary(va_list ap, params_t *params)
 	params->unsign = 1;
 	return (c += print_number(str, params));
 }
+
 /**
  * print_octal - prints octal numbers
  * @ap: p1
@@ -81,7 +84,7 @@ int print_binary(va_list ap, params_t *params)
 int print_octal(va_list ap, params_t *params)
 {
 	unsigned long l;
-	char *string;
+	char *str;
 	int c = 0;
 
 	if (params->l_modifier)
@@ -90,10 +93,10 @@ int print_octal(va_list ap, params_t *params)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
-	string = convert(l, 8, CONVERT_UNSIGNED, params);
+	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
 	if (params->hashtag_flag && l)
-		*--string = '0';
+		*--str = '0';
 	params->unsign = 1;
-	return (c += print_number(string, params));
+	return (c += print_number(str, params));
 }
